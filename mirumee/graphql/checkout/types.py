ea5ee05209bd64779e8335975d730f2c0f5e394a
Mateusz Koczorowski <mat.kocz00@gmail.com>
2021-06-15 20:34:52 +0200

    Day 6

--- mirumee/graphql/checkout/types.py
+++ mirumee/graphql/checkout/types.py
@@ -1,7 +1,6 @@
 from graphene_django import DjangoObjectType
-import graphene
+
 from ...checkout.models import Checkout, CheckoutLine
-from django.db.models import Sum
 
 
 class CheckoutType(DjangoObjectType):
@@ -9,20 +8,8 @@ class CheckoutType(DjangoObjectType):
         model = Checkout
         fields = "__all__"
 
-    def resolve_total_price(self, _info):
-        checkout = self.lines.all().aggregate(sum_checkout_price=Sum('variant__price'))
-        sum_checkout_price = checkout['sum_checkout_price']
-        if not sum_checkout_price:
-            return self.lines
 
 class CheckoutLineType(DjangoObjectType):
     class Meta:
         model = CheckoutLine
         fields = "__all__"
-
-    def resolve_total_price(self, _info):
-        checkout_line_price = self.variant.price*self.quantity
-        if not checkout_line_price:
-            return self.variants
-
-        return checkout_line_price
--- mirumee/graphql/product/mutations.py
+++ mirumee/graphql/product/mutations.py
@@ -1,19 +1,15 @@
+
 import graphene
+
 from .types import ProductType, ProductVariantType
 from ...product.models import Product, ProductVariant
-
+from ..core.utils import staff_member_required, superuser_required
 
 class ProductCreateInput(graphene.InputObjectType):
     name = graphene.String(required=True)
     price = graphene.Decimal(required=True)
     description = graphene.String(required=True)
     quantity = graphene.Int()
-
-
-class ProductVariantCreateInput(graphene.InputObjectType):
-    name = graphene.String(required=True)
-    sku = graphene.String(required=True)
-    price = graphene.Decimal(required=True)
 
 
 class ProductCreate(graphene.Mutation):
@@ -24,15 +20,23 @@ class ProductCreate(graphene.Mutation):
 
     @classmethod
     def clean_input(cls, input):
+        # TODO price should be Decimal
         return input
 
     @classmethod
-    def mutate(cls, root, _info, input):
+    @staff_member_required
+    def mutate(cls, root, info, input):
         cleaned_input = cls.clean_input(input)
 
         product = Product.objects.create(**cleaned_input)
 
         return ProductCreate(product=product)
+
+
+class ProductVariantCreateInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    sku = graphene.String(required=True)
+    price = graphene.Decimal(required=True)
 
 
 class ProductVariantCreate(graphene.Mutation):
@@ -43,10 +47,19 @@ class ProductVariantCreate(graphene.Mutation):
         product_id = graphene.ID(required=True)
 
     @classmethod
-    def clean_input(cls, input):
-        return input
+    def clean_price(cls):
+        pass
 
     @classmethod
+    def clean_input(cls, data):
+        cls.clean_price()
+        # sprawdzic czy cena nie jest ujemna
+        # sprawdzic czy produkt o podanym ID istnieje
+        # zlapac wyjatek gdy istnieje juz variant o podanym SKU
+        return data
+
+    @classmethod
+    @staff_member_required
     def mutate(cls, root, _info, input, product_id):
         cleaned_input = cls.clean_input(input)
         product_variant = ProductVariant.objects.create(product_id=product_id, **cleaned_input)
